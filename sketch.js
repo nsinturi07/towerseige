3ef577d23b22eb20bf0556ad7e9e586779558709
nsinturi07 <65034759+nsinturi07@users.noreply.github.com>
2020-07-16 20:18:13 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const constraint = Matter.Constraint;
+var box1, box2, box3, box4, box5;
+var chain;
+
+
+function setup() {
+  createCanvas(1200,400);
+  createSprite(400, 200, 50, 50);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(600,400,1200,20);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    box5 = new Box(810,160,70,70);
+    paper = new paperObject(50,50,70);
+    chain = new Slingshot(paper.body,{x:200,y:100});
+    Engine.run(engine);
+}
+
+function draw() {
+  background(220); 
+  Engine.update(engine);
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  paper.display();
+  chain.display();
+
+
+  drawSprites();
+}
+function mouseDragged(){
+  //if (gameState!=="launched"){
+      Matter.Body.setPosition(paper.body, {x: mouseX , y: mouseY});
+  //}
+}
+
+
+function mouseReleased(){
+  chain.fly();
+  
+}
